41c380d48979758587337a933762d207cea0b9bd
Maxwell Lovell <maxwellloganlovell@gmail.com>
2021-11-29 12:12:22 -0500

    [Update] Added fade in between tour spots and buttons for rest of tourspots

--- Assets/Scripts/TourChange.cs
+++ Assets/Scripts/TourChange.cs
@@ -8,10 +8,14 @@ public class TourChange : MonoBehaviour
     public GameObject tourCurrent;
     public GameObject tourNext;
 
+    public Animator animator;
+
     public void TourTransition()
     {
+        animator.SetTrigger("FadeOut");
         tourNext.SetActive(true);
         player.transform.position = tourNext.transform.position;
+        player.transform.position -= new Vector3(0f, 2f);
         tourCurrent.SetActive(false);
     }
 }
